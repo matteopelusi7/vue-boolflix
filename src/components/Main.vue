85b3dc6858874181eee315d0cb6b94836f160e8a
matteopelusi7 <pelusimatteo02@gmail.com>
2022-03-15 17:14:52 +0100

    Add milestone 2

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,8 +10,17 @@
       <li v-for="film in films" :key="film.id" class="list-item">
         <p>{{ film.title }}</p>
         <p>{{ film.original_title }}</p>
-        <p>{{ film.original_language }}</p>
+        <img :src="film.original_language" alt="">
         <p>{{ film.vote_average }}</p>
+      </li>
+    </ul>
+
+    <ul class="list-wrapper">
+      <li v-for="serie in series" :key="serie.id" class="list-item">
+        <p>{{ serie.name }}</p>
+        <p>{{ serie.original_name }}</p>
+        <img :src="serie.original_language" alt="">
+        <p>{{ serie.vote_average }}</p>
       </li>
     </ul>
 
@@ -27,11 +36,13 @@ export default {
   data() {
     return {
       search: '',
-      films: []
+      films: [],
+      series: []
     }
   },
   methods: {
     getFilms: function () {
+
       axios.get('https://api.themoviedb.org/3/search/movie', {
         params: {
           api_key: 'f856965414a471b0188348e3edefb8c6',
@@ -41,12 +52,67 @@ export default {
       })
       .then( res => {
         this.films = res.data.results
+
+        this.films.forEach( el => {
+
+          switch(el.original_language) {
+            case 'en':
+              el.original_language = require('../assets/img/england.webp')
+              break;
+            case 'it':
+              el.original_language = require('../assets/img/italia.webp')
+              break;
+            case 'es':
+              el.original_language = require('../assets/img/spagna.webp')
+            break;
+            default:
+              el.original_language = require('../assets/img/default.webp')
+          }
+
+        });
+
       })
       .catch( error => {
         console.log(error.response)
       })
+
+      axios.get('https://api.themoviedb.org/3/search/tv', {
+        params: {
+          api_key: 'f856965414a471b0188348e3edefb8c6',
+          query: this.search,
+          language: 'it-IT'
+        }
+      })
+      .then( res => {
+        this.series = res.data.results
+
+        this.series.forEach( el => {
+
+          switch(el.original_language) {
+            case 'en':
+              el.original_language = require('../assets/img/england.webp')
+              break;
+            case 'it':
+              el.original_language = require('../assets/img/italia.webp')
+              break;
+            case 'es':
+              el.original_language = require('../assets/img/spagna.webp')
+            break;
+            default:
+              el.original_language = require('../assets/img/default.webp')
+          }
+
+        });
+
+      })
+      .catch( error => {
+        console.log(error.response)
+      })
+
     }
-  }
+  
+  },
+
 }
 
 </script>
@@ -58,6 +124,10 @@ main {
 
   .list-wrapper{
     padding: 10px 0;
+
+    img {
+      width: 15px;
+    }
 
     .list-item {
       padding: 15px 0;
